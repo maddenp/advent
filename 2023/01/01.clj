57b9876bb09490926542af66c66eb51e8ed7dc49
Paul Madden <paul.madden@noaa.gov>
2023-12-02 00:55:22 +0000

    Simplification

--- 2023/01/01.clj
+++ 2023/01/01.clj
@@ -1,28 +1,26 @@
 (require '[clojure.string :as s])
 
-(def numbers ["zero" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine"])
-
-(defn digit? [c] (<= (int \0) (int c) (int \9)))
-
-(defn mark
+(defn mark-one
   [word]
-  (->> (for [[k v] (zipmap numbers (range (count numbers)))]
-         (when (s/starts-with? word k)
-           (apply str v (subs word 1))))
-       (filter some?)
-       first))
+  (let [numbers ["zero" "one" "two" "three" "four" "five" "six" "seven" "eight" "nine"]]
+    (->> (for [[k v] (zipmap numbers (range (count numbers)))]
+           (when (s/starts-with? word k)
+             (apply str v (subs word 1))))
+         (filter some?)
+         first)))
 
 (defn mark-all
   [word]
   (loop [word word marked ""]
     (if (< (count word) 1)
       marked
-      (let [w (or (mark word) word)]
+      (let [w (or (mark-one word) word)]
         (recur (subs w 1) (str marked (first w)))))))
 
 (defn part1
   [text]
-  (let [f #(Integer/parseInt (let [d (filter digit? %)] (str (first d) (last d))))]
+  (let [digit? #(<= (int \0) (int %) (int \9))
+        f #(Integer/parseInt (let [d (filter digit? %)] (str (first d) (last d))))]
     (apply + (map f (s/split text #"\n")))))
 
 (defn part2
