ddc50ce81753ac57421b24da658186d5d2d48784
Paul Madden <maddenp@colorado.edu>
2023-12-02 18:48:10 +0000

    WIP on 02

---
+++ 2023/02.clj
@@ -0,0 +1,33 @@
+#_(require '[clojure.pprint :only pprint])
+(require '[clojure.string :as s])
+
+(def input (s/split (slurp "02.txt") #"\n"))
+
+(defn draw-maps
+  [draw-str]
+  (apply merge
+         (map #(let [[n c] (s/split % #" ")] {c (Integer/parseInt n)})
+              (s/split draw-str #", "))))
+
+(defn game-map
+  [game-str]
+  (let [[_ n draws] (re-matches #"^Game (\d+): (.*)$" game-str)]
+    {:n n :draws (map draw-maps (s/split draws #"; "))}))
+
+(defn valid-draw
+  [draw]
+  (let [max {"red" 12 "green" 13 "blue" 14}]
+    (every? true? (map #(<= (draw %) (max %)) (keys draw)))))
+
+(defn valid-game
+  [game]
+  (every? true? (map valid-draw (:draws game))))
+
+#_(pprint (map game-map input))
+
+(def games (map game-map input))
+
+(first games)
+(valid-game (first games))
+(last games)
+(valid-game (last games))
