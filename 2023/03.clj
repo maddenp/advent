8b209b185b70b8489539bb64fa50c0ecd2fdb8dd
Paul Madden <maddenp@colorado.edu>
2023-12-03 20:50:17 +0000

    WIP on 02 part 2

--- 2023/03.clj
+++ 2023/03.clj
@@ -2,7 +2,7 @@
          '[clojure.set :refer [difference]]
          '[clojure.string :as s])
 
-(declare digit? halo)
+(declare halo runs)
 
 (defn at [a [r c]] (try (aget a r c) (catch Exception _ \.)))
 (defn cols [a] (alength (aget a 0)))
@@ -25,6 +25,22 @@
                                   (<= 0 hr (dec (rows a)))
                                   (<= 0 hc (dec (cols a))))))))
 
+(defn numbers
+  [a]
+  (for [run (runs a)]
+    (let [rcs (map :rc run)]
+      {:n (->> (map #(at a %) rcs)     ; coords -> chars
+               (apply str)             ; combine chars -> str
+               Integer/parseInt)       ; str -> int
+       :adj (as-> (map :halo run) $    ; get all neighbor coords
+              (reduce into [] $)       ; all coords in one vector
+              (set $)                  ; remove duplicates
+              (difference $ (set rcs)) ; remove coords belonging to number
+              (map #(at a %) $)        ; coords -> chars
+              (set $)                  ; remove duplicates
+              (filter #(not= \. %) $)  ; remove '.'s
+              )}))) 
+
 (defn runs
   [a]
   (loop [ds (digits a) ns [] n nil]
@@ -35,21 +51,6 @@
           (and (= r nr) (= c (inc nc))) (recur (rest ds) ns (conj n d))
           :else (recur (rest ds) (conj ns n) [d])))
       (conj ns n))))
-
-(defn numbers
-  [a]
-  (for [run (runs a)]
-    (let [rcs (map :rc run)]
-      {:n (->> (map #(at a %) rcs)
-               (apply str)
-               Integer/parseInt)
-       :adj (as-> (map :halo run) $
-              (reduce into [] $)
-              (set $)
-              (difference $ (set rcs))
-              (map #(at a %) $)
-              (set $)
-              (filter #(not= \. %) $))})))
 
 (require '[clojure.pprint :refer [pprint]])
 (pprint (numbers (to-array-2d ["467..114.."
