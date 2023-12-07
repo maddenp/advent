8d03ab03696ec4777e2a3ccefc824221abd520e7
Paul Madden <paul.madden@noaa.gov>
2023-12-07 20:07:57 +0000

    WIP on 07 part 2

--- 2023/07.clj
+++ 2023/07.clj
@@ -1,6 +1,7 @@
-(require '[clojure.string :as s])
+(require '[clojure.math.combinatorics :refer [cartesian-product]]
+         '[clojure.string :as s])
 
-(def hands (s/join "\n" ["32T3K 765"
+(def input (s/join "\n" ["32T3K 765"
                          "T55J5 684"
                          "KK677 28"
                          "KTJJT 220"
@@ -11,15 +12,27 @@
   (let [s {[5] 7 [1 4] 6 [2 3] 5 [1 1 3] 4 [1 2 2] 3 [1 1 1 2] 2 [1 1 1 1] 1}]
     (s (vec (sort (vals (frequencies hand)))))))
 
+(defn best
+  [cards hand]
+  (->> (repeat ((frequencies hand) \J 0) cards)
+       (apply cartesian-product)
+       (map #(apply format (s/replace hand #"J" "%s") %))
+       (map strength)
+       (apply max)))
+
 (defn quantify
   [cards hand]
   (mapv #((zipmap cards (range 2 (+ 2 (count cards)))) %) hand))
 
 (defn s->i [s] (Long/parseLong s))
 
-(defn augment
+(defn augment1
   [cards hand bid]
   {:strength (strength hand) :hand (quantify cards hand) :bid (s->i bid)})
+
+(defn augment2
+  [cards hand bid]
+  {:strength (best (rest cards) hand) :hand (quantify cards hand) :bid (s->i bid)})
 
 (defn handcomp
   [h1 h2]
@@ -33,7 +46,7 @@
   (let [cards [\J \2 \3 \4 \5 \6 \7 \8 \9 \T \Q \K \A]]
     (as-> (s/split hands #"\n") $
       (map #(s/split % #" ") $)
-      (map #(augment cards (first %) (last %)) $)
+      (map #(augment2 cards (first %) (last %)) $)
       (sort handcomp $)
       (map vector (map #(:bid %) $) (range 1 (inc (count $))))
       (map #(apply * %) $)
@@ -44,11 +57,11 @@
   (let [cards [\2 \3 \4 \5 \6 \7 \8 \9 \T \J \Q \K \A]]
     (as-> (s/split hands #"\n") $
       (map #(s/split % #" ") $)
-      (map #(augment cards (first %) (last %)) $)
+      (map #(augment1 cards (first %) (last %)) $)
       (sort handcomp $)
       (map vector (map #(:bid %) $) (range 1 (inc (count $))))
       (map #(apply * %) $)
       (apply + $))))
 
-(let [input (slurp "07.txt")]
-  (println (part1 input)))
+(let [input input #_(slurp "07.txt")]
+  (println #_(part1 input) (part2 input)))
