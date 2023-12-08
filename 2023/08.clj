5ac11c5d1dc17e1939ebb628eafe4c1c3d793b04
Paul Madden <paul.madden@noaa.gov>
2023-12-08 16:56:43 +0000

    Solves 08 part 1

--- 2023/08.clj
+++ 2023/08.clj
@@ -6,9 +6,22 @@
                          "BBB = (AAA, ZZZ)"
                          "ZZZ = (ZZZ, ZZZ)"]))
 
+(defn prep
+  [input]
+  (let [[lr nodes] (s/split input #"\n\n")]
+    (as-> (s/split nodes #"\n") $
+      (map #(s/split % #" = ") $)
+      (map #(hash-map (% 0) (zipmap [\L \R] (rest (re-matches #"^\((\w+), (\w+)\)$" (% 1))))) $)
+      (apply merge $)
+      [lr $])))
+
 (defn part1
   [input]
-  input)
+  (let [[lr nodes] (prep input) m (count lr)]
+    (loop [n 0 node "AAA"]
+      (if (= node "ZZZ")
+        n
+        (recur (inc n) ((nodes node) (nth lr (mod n m))))))))
 
-(let [input input #_(slurp "08.txt")]
+(let [input #_input (slurp "08.txt")]
   (println (part1 input)))
