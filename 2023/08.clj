1904d9dd95db4a8c8f4d1b24852261df3c76e5ec
Paul Madden <paul.madden@noaa.gov>
2023-12-09 02:33:57 +0000

    WIP on 08 part 2

--- 2023/08.clj
+++ 2023/08.clj
@@ -6,17 +6,6 @@
                           "BBB = (AAA, ZZZ)"
                           "ZZZ = (ZZZ, ZZZ)"]))
 
-(def input2 (s/join "\n" ["LR"
-                          ""
-                          "11A = (11B, XXX)"
-                          "11B = (XXX, 11Z)"
-                          "11Z = (11B, XXX)"
-                          "22A = (22B, XXX)"
-                          "22B = (22C, 22C)"
-                          "22C = (22Z, 22Z)"
-                          "22Z = (22B, 22B)"
-                          "XXX = (XXX, XXX)"]))
-
 (defn prep
   [input]
   (let [[lr nodes] (s/split input #"\n\n")]
@@ -35,12 +24,34 @@
         (recur (inc n) ((nodes x) (nth lr (mod n m))))))))
 
 (defn part2
-    [input]
-    (let [[lr nodes] (prep input) xs (filter #(s/ends-with? % "A") (keys nodes))]
-      xs
-      ))
+  [input]
+  (let [[lr nodes] (prep input) m (count lr)]
+    (loop [n 0 xs (filter #(s/ends-with? % "A") (keys nodes))]
+      (if (every? #(s/ends-with? % "Z") xs)
+        n
+        (recur (inc n) (let [i (nth lr (mod n m))] (vec (map (fn [x] ((nodes x) i)) xs))))
+        ))))
 
-(require '[clojure.pprint :refer [pprint]])
 (let [input (slurp "08.txt")]
-  (pprint (part2 input2))
+  (println (part2 input))
   #_(println (part1 input1) (part2 input2)))
+
+(def input2 (s/join "\n" ["LR"
+                          ""
+                          "11A = (11B, XXX)"
+                          "11B = (XXX, 11Z)"
+                          "11Z = (11B, XXX)"
+                          "22A = (22B, XXX)"
+                          "22B = (22C, 22C)"
+                          "22C = (22Z, 22Z)"
+                          "22Z = (22B, 22B)"
+                          "XXX = (XXX, XXX)"]))
+
+#_(let [[lr nodes] (prep #_input2 (slurp "08.txt"))
+      next (fn [x n] ((nodes x) (nth lr (mod n (count lr)))))
+      xs (filter #(s/ends-with? % "A") (keys nodes))]
+  (loop [x "GFA" n 0 foo [] seen #{}]
+    (println "@@@" x)
+    (if (s/ends-with? x "Z")
+      (do (println "***" x n foo) n)
+      (recur (next x n) (inc n) (conj foo x) (conj seen x)))))
