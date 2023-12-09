ab6881217317eb3446d1170d29de029a86591301
Paul Madden <paul.madden@noaa.gov>
2023-12-09 21:10:21 +0000

    WIP on 08 part 2

--- 2023/08.clj
+++ 2023/08.clj
@@ -23,19 +23,6 @@
         n
         (recur (inc n) ((nodes x) (nth lr (mod n m))))))))
 
-(defn part2
-  [input]
-  (let [[lr nodes] (prep input) m (count lr)]
-    (loop [n 0 xs (filter #(s/ends-with? % "A") (keys nodes))]
-      (if (every? #(s/ends-with? % "Z") xs)
-        n
-        (recur (inc n) (let [i (nth lr (mod n m))] (vec (map (fn [x] ((nodes x) i)) xs))))
-        ))))
-
-#_(let [input (slurp "08.txt")]
-    (println (part2 input))
-    #_(println (part1 input1) (part2 input2)))
-
 (def input2 (s/join "\n" ["LR"
                           ""
                           "11A = (11B, XXX)"
@@ -47,20 +34,20 @@
                           "22Z = (22B, 22B)"
                           "XXX = (XXX, XXX)"]))
 
-;;       xs (filter #(s/ends-with? % "A") (keys nodes))]
+;; (filter #(s/ends-with? % "A") (keys nodes))]
 
 (defn z? [s] (s/ends-with? s "Z"))
 
 (defn get-cycle
   [lr nodes start]
-  (let [m #(mod % (count lr))
-        next (fn [x n] ((nodes x) (nth lr (m n))))]
+  (let [pos #(mod % (count lr))
+        next (fn [x n] ((nodes x) (nth lr (pos n))))]
     (loop [x start n 0 path [] seen #{}]
-      (if (seen [x (m n)])
+      (if (seen [x (pos n)])
         path
-        (recur (next x n) (inc n) (conj path [x n]) (conj seen [x (m n)]))))))
+        (recur (next x n) (inc n) (conj path [x n]) (conj seen [x (pos n)]))))))
 
 (let [[lr nodes] (prep input2 #_(slurp "08.txt"))
-      cycle (get-cycle lr nodes "11A")
+      cycle (get-cycle lr nodes "22A")
       a (last (last cycle))]
   (println (map #(vector a (last %)) (filter #(z? (first %)) cycle))))
