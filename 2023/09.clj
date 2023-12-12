ef1c7c43ed2b06d609c9c3d61da06e2b5f955b64
Paul Madden <paul.madden@noaa.gov>
2023-12-12 06:12:53 +0000

    Cleanup

--- 2023/09.clj
+++ 2023/09.clj
@@ -1,9 +1,5 @@
 (require '[clojure.string :as s])
 
-(def input (s/join "\n" ["0 3 6 9 12 15"
-                         "1 3 6 10 15 21"
-                         "10 13 16 21 30 45"]))
-
 (defn extrapolate
   [history]
   (if (every? zero? history)
@@ -18,9 +14,13 @@
   [histories]
   (apply + (map extrapolate histories)))
 
-(let [input #_input (slurp "09.txt")
+(defn part2
+  [histories]
+  (apply + (map extrapolate (map reverse histories))))
+
+(let [input (slurp "09.txt")
       histories (as-> input $
                   (s/split $ #"\n")
                   (map #(s/split % #"\s+" ) $)
                   (map #(map (fn [x] (Long/parseLong x)) %) $))]
-  (println (part1 histories)))
+  (println (part1 histories) (part2 histories)))
