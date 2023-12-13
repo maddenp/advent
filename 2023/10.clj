d7ca8158d1dd7f350b271a8392dfa346c919071d
Paul Madden <paul.madden@noaa.gov>
2023-12-13 17:27:02 +0000

    Cleanup

--- 2023/10.clj
+++ 2023/10.clj
@@ -20,65 +20,65 @@
    \╗ {:n #{        } :e #{        } :s #{\║ \╚ \╝} :w #{\═ \╚ \╔}}
    \╔ {:n #{        } :e #{\═ \╝ \╗} :s #{\║ \╚ \╝} :w #{        }}})
 
-(defn at [a [r c]] (try (aget a r c) (catch Exception _ \.)))
+(defn at [arr [r c]] (try (aget arr r c) (catch Exception _ \.)))
 (defn char->pipe [c] (or (get (zipmap [\| \- \L \J \7 \F] pipes) c) c))
-(defn cols [a] (alength (aget a 0)))
-(defn rows [a] (alength a))
+(defn cols [arr] (alength (aget arr 0)))
+(defn rows [arr] (alength arr))
 (defn fits? [c con dir] (((fittings c) dir) con))
 
 #_(defn show
-    [a]
-    (doseq [row (range (rows a))]
-      (doseq [col (range (cols a))]
-        (print (aget a row col)))
+    [arr]
+    (doseq [row (range (rows arr))]
+      (doseq [col (range (cols arr))]
+        (print (aget arr row col)))
       (println)))
 
 (defn coords->neighbors
-  [a coords]
+  [arr coords]
   (for [dir dirs]
     (let [offset (offsets dir) [r c] coords]
-      (at a [(+ r (get offset 0))
-             (+ c (get offset 1))]))))
+      (at arr [(+ r (get offset 0))
+               (+ c (get offset 1))]))))
 
 (defn plausible-neighbors
   [pipe neighbors]
   (apply union (map #(intersection ((fittings pipe) %) (set [(neighbors %)])) dirs)))
 
 (defn s->coords
-  [a]
-  (->> (cartesian-product (range (rows a)) (range (cols a)))
-       (filter #(= \S (at a %)))
+  [arr]
+  (->> (cartesian-product (range (rows arr)) (range (cols arr)))
+       (filter #(= \S (at arr %)))
        first))
 
 (defn s->pipe
-  [a s]
-  (let [neighbors (zipmap dirs (coords->neighbors a s))]
+  [arr s]
+  (let [neighbors (zipmap dirs (coords->neighbors arr s))]
     (->> pipes
          (map #(vector % (plausible-neighbors % neighbors)))
          (filter #(= 2 (count (last %))))
          first
          first)))
 
-(defn to-visit
-  [a x d]
+(defn neighbors-to-visit
+  [arr x d]
   (remove nil?
           (for [dir dirs]
             (let [offset (offsets dir)
                   coords [(+ (first x) (first offset))
                           (+ (last x) (last offset))]]
-              (when (and (fits? (at a x) (at a coords) dir)
+              (when (and (fits? (at arr x) (at arr coords) dir)
                          (or (not (d coords)) (> (d coords) (inc (d x)))))
                 coords)))))
 
 (defn part1
-  [a]
-  (let [s (s->coords a)]
-    (aset a (first s) (last s) (s->pipe a s))
+  [arr]
+  (let [s (s->coords arr)]
+    (aset arr (first s) (last s) (s->pipe arr s))
     (loop [d {s 0} q (reduce conj clojure.lang.PersistentQueue/EMPTY [s])]
       (if (empty? q)
         (apply max (vals d))
         (let [x (peek q)
-              v (to-visit a x d)]
+              v (neighbors-to-visit arr x d)]
           (recur (merge d (zipmap v (repeat (inc (d x)))))
                  (apply conj (pop q) v)))))))
 
