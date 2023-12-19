82f0e95f40369da9f30356f02b5b817a0ada055c
Paul Madden <paul.madden@noaa.gov>
2023-12-19 16:38:10 +0000

    Solves 11.2

--- 2023/11.clj
+++ 2023/11.clj
@@ -1,6 +1,8 @@
 (require '[clojure.math.combinatorics :refer [combinations]]
          '[clojure.string :as s])
 
+(declare lines)
+
 (def input (s/join "\n" ["...#......"
                          ".......#.."
                          "#........."
@@ -18,41 +20,53 @@
 
 (defn galaxies
   [image]
-  (for [[r row] (map-indexed vector (s/split image #"\n"))
+  (for [[r row] (map-indexed vector (lines image))
         [c sym] (map-indexed vector row)
         :when (= sym \#)]
     [r c]))
+
+(defn lines
+  [image]
+  (s/split image #"\n"))
 
 (defn manhattan-distance
   [[r1 c1] [r2 c2]]
   (+ (abs (- r1 r2)) (abs (- c1 c2))))
 
+(defn pairs
+  [galaxies]
+  (combinations galaxies 2))
+
 (defn transpose
   [s]
-  (->> (s/split s #"\n")
+  (->> (lines s)
        (map #(seq (char-array %)))
        (apply mapv vector)
        (map #(apply str %))
        (s/join "\n")))
 
 (defn part1
-  [input]
-  (as-> input $
-        (expand $)
-        (transpose $)
-        (expand $)
-        (transpose $)
-        (galaxies $)
-        (combinations $ 2)
-        (map (fn [[g1 g2]] (manhattan-distance g1 g2)) $)
-        (apply + $)))
+  [image]
+  (->> image
+       expand
+       transpose
+       expand
+       transpose
+       galaxies
+       pairs
+       (map (fn [[g1 g2]] (manhattan-distance g1 g2)))
+       (apply +)))
 
 (defn part2
-  [input]
-  (let [blanks (fn [x] (count (filter #(re-matches #"^\.+$" %) (s/split x #"\n"))))
-        blank-rows (blanks input)
-        blank-cols (blanks (transpose input))]
-    blank-cols))
+  [image]
+  (let [walk #(apply range (sort [%1 %2]))
+        dist #(if (re-matches #"^\.+$" %) 1000000 1)
+        rdist (mapv dist (lines image))
+        cdist (mapv dist (lines (transpose image)))
+        gs (->> image galaxies sort pairs)]
+    (apply + (for [[[r1 c1] [r2 c2]] gs]
+               (apply + (flatten [(map #(rdist %) (walk r1 r2))
+                                  (map #(cdist %) (walk c1 c2))]))))))
 
-(let [input input #_(slurp "11.txt")]
+(let [input #_input (slurp "11.txt")]
   (println #_(part1 input) (part2 input)))
