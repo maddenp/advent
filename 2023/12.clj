b259d76bdabb5779d81b8d3774538d58e825bcc7
Paul Madden <paul.madden@noaa.gov>
2023-12-20 05:43:42 +0000

    WIP on 12.1

--- 2023/12.clj
+++ 2023/12.clj
@@ -12,29 +12,29 @@
   (println "@@@" springs groups)
   (read-line)
   (let [fs (first springs) rs (rest springs) fg (first groups) rg (rest groups)]
-    (if (empty? groups)
-      (do (println "groups empty, returning 1") 1)
-      (if (empty? springs)
-        (do (println "springs empty, returning 0") 0)
-        (if (= fg 0)
-          (if (or (= fs \.) (= fs \?))
-            (do (println "need ." fs "ok") (f rs rg))
-            (do (println "need ." fs "bad returning 0") 0))
-          (cond (= fs \?)
-                (+ (do (println "0 branch" springs groups) (f rs groups))
-                   (do (println "1 branch" springs groups) (f rs (apply conj [(dec fg)] rg))))
-                (= fs \.)
-                (do (println "skipping .") (f rs groups))
-                (= fs \#)
-                (do (println "matching #") (f rs (apply conj [(dec fg)] rg)))
-                ))))))
-  
-  (defn one
-    [record]
-    (let [[a b] (s/split record #"\s")
-          springs (vec a)
-          groups (map #(Long/parseLong %) (s/split b #","))]
-      (f springs groups)))
+    (if (empty? springs)
+      (if (= fg 0)
+        (do (println "groups empty, returning 1") 1)
+        (do (println "springs empty, returning 0") 0))
+      (if (= fg 0)
+        (if (or (= fs \.) (= fs \?))
+          (do (println "need ." fs "ok") (f rs rg))
+          (do (println "need ." fs "bad returning 0") 0))
+        (cond (= fs \?)
+              (+ (do (println "0 branch" springs groups) (f rs groups))
+                 (do (println "1 branch" springs groups) (f rs (apply conj [(dec fg)] rg))))
+              (= fs \.)
+              (do (println "skipping .") (f rs groups))
+              (= fs \#)
+              (do (println "matching #") (f rs (apply conj [(dec fg)] rg)))
+              )))))
 
-  (let [records (s/split input #"\n")]
-    (println (one (first records))))
+(defn one
+  [record]
+  (let [[a b] (s/split record #"\s")
+        springs (vec a)
+        groups (map #(Long/parseLong %) (s/split b #","))]
+    (f springs groups)))
+
+(let [records (s/split input #"\n")]
+  (println (one (first records))))
