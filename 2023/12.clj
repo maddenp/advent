63a913ea218afb0e0d72f52cc910956ec93ecbe4
Paul Madden <paul.madden@noaa.gov>
2023-12-21 04:19:17 +0000

    WIP on 12.1

--- 2023/12.clj
+++ 2023/12.clj
@@ -8,25 +8,26 @@
                          "?###???????? 3,2,1"]))
 
 (defn f
-  [springs groups]
-  (println "@@@" springs groups)
-  #_(read-line)
+  [springs groups line]
+  (println "@@@" springs groups (str \" line \"))
+  (read-line)
   (let [fs (first springs) rs (rest springs) fg (first groups) rg (rest groups)]
     (if (empty? springs)
-      (if (or (nil? fg) (= fg 0))
-        (do (println "groups empty, returning 1") 1)
+      (if (or (nil? fg) (= groups [0]))
+        (do (println "groups empty, RETURNING 1 FOR" (str \" line \")) 1)
         (do (println "springs empty, returning 0") 0))
       (if (= fg 0)
         (if (or (= fs \.) (= fs \?))
-          (do (println "need ." fs "ok") (f rs rg))
+          (do (println "need . " (str "(accepting " fs ")")) (f rs rg (str line ".")))
           (do (println "need ." fs "bad returning 0") 0))
         (cond (= fs \?)
-              (+ (do (println "0 branch" springs groups) (f rs groups))
-                 (do (println "1 branch" springs groups) (f rs (apply conj [(dec fg)] rg))))
+              (+ (do (println ". branch" springs groups) (f rs groups (str line ".")))
+                 (do (println "# branch" springs groups)
+                     (if fg (f rs (apply conj [(dec fg)] rg) (str line "#")) 0)))
               (= fs \.)
-              (do (println "skipping .") (f rs groups))
+              (do (println "skipping .") (f rs groups (str line ".")))
               (= fs \#)
-              (do (println "matching #") (f rs (apply conj [(dec fg)] rg)))
+              (do (println "matching #") (f rs (apply conj [(dec fg)] rg) (str line "#")))
               )))))
 
 (defn one
@@ -34,7 +35,7 @@
   (let [[a b] (s/split record #"\s")
         springs (vec a)
         groups (map #(Long/parseLong %) (s/split b #","))]
-    (f springs groups)))
+    (f springs groups "")))
 
 (let [records (s/split input #"\n")]
   (println (one (nth records 5))))
