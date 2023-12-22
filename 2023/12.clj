3637197178c26cbbf510f892206e822dd4415b4b
Paul Madden <paul.madden@noaa.gov>
2023-12-22 01:42:18 +0000

    Cleanup

--- 2023/12.clj
+++ 2023/12.clj
@@ -10,16 +10,16 @@
 (defn f
   [springs groups run]
   (let [fs (first springs) rs (rest springs) fg (first groups) rg (rest groups)
-        damaged #(f rs (apply conj [(dec fg)] rg) true)
-        operational #(f rs groups false)]
+        hash #(f rs (apply conj [(dec fg)] rg) true)
+        dot #(f rs groups false)]
     (if (empty? springs)
       (case groups ([] [0]) 1 0)
       (if (= fg 0)
         (case fs (\. \?) (f rs rg false) 0)
         (case fs
-          \# (if fg (damaged) 0)
-          \. (if run 0 (operational))
-          \? (+ (if run 0 (operational)) (if fg (damaged) 0)))))))
+          \# (if fg (hash) 0)
+          \. (if run 0 (dot))
+          \? (+ (if run 0 (dot)) (if fg (hash) 0)))))))
 
 (defn one
   [record]
