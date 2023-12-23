7ae563028633b23d6b0a7fbafb1d8cddd1e2627f
Paul Madden <paul.madden@noaa.gov>
2023-12-23 05:41:31 +0000

    Solves 13.1

--- 2023/13.clj
+++ 2023/13.clj
@@ -16,6 +16,16 @@
                          "..##..###"
                          "#....#..#"]))
 
+#_(def input (s/join "\n" [".#....###"
+                         "#.#######"
+                         "#.#######"
+                         ".#....###"
+                         "..##.#..."
+                         ".###.#.##"
+                         ".#...#.##"
+                         "..####.##"
+                         "...##..#."]))
+
 (defn transpose
   [pattern]
   (->> (s/split pattern #"\n")
@@ -24,8 +34,8 @@
        (map #(apply str %))
        (s/join "\n")))
 
-(defn f
-  [c reject lines]
+(defn fwd
+  [c lines]
   #_(print c)
   #_(pprint lines)
   (if (seq lines)
@@ -35,44 +45,57 @@
             #_(print "b") #_(pprint (reverse (subvec lines (/ n 2))))
             (if (= (subvec lines 0 (/ n 2)) (reverse (subvec lines (/ n 2))))
               (+ c (/ n 2))
-              (f (+ c 2) reject (vec (reject 2 lines))))
+              (fwd (+ c 2) (vec (drop 2 lines))))
             )
-        (f (+ c 1) reject (vec (reject 1 lines)))))
+        (fwd (+ c 1) (vec (drop 1 lines)))))
     0))
 
-(def fwd (partial f 0 drop))
-(def rev (partial f 0 drop-last))
+(defn rev
+  [c lines]
+  #_(print c)
+  #_(pprint lines)
+  (if (seq lines)
+    (let [n (count lines)]
+      (if (= (mod n 2) 0)
+        (do #_(print "a") #_(pprint (subvec lines 0 (/ n 2)))
+            #_(print "b") #_(pprint (reverse (subvec lines (/ n 2))))
+            (if (= (subvec lines 0 (/ n 2)) (reverse (subvec lines (/ n 2))))
+              (/ n 2)
+              (rev (+ c 2) (vec (drop-last 2 lines))))
+            )
+        (rev (+ c 1) (vec (drop-last 1 lines)))))
+    0))
 
 (require '[clojure.pprint :refer [pprint]])
 
 (defn rows
   [pattern]
   (let [lines (s/split pattern #"\n")]
-    (println "rows")
-    (pprint lines)
-    (* 100 (+ (fwd lines)
-              (if (= lines (reverse lines)) 0 (rev lines))))))
+    #_(println "rows")
+    #_(pprint lines)
+    (* 100 (+ (fwd 0 lines)
+              (if (= lines (reverse lines)) 0 (rev 0 lines))))))
 
 (defn cols
   [pattern]
   (let [lines (s/split (transpose pattern) #"\n")]
-    (println "cols")
-    (pprint lines)
-    (+ (fwd lines)
-       (if (= lines (reverse lines)) 0 (rev lines)))))
+    #_(println "cols")
+    #_(pprint lines)
+    (+ (fwd 0 lines)
+       (if (= lines (reverse lines)) 0 (rev 0 lines)))))
 
 (defn score
   [pattern]
   (let [sr (rows pattern)
         sc (cols pattern)]
-    (println "rows" sr "cols" sc)
-    (read-line)
+    #_(println "rows" sr "cols" sc)
+    #_(read-line)
     (+ (rows pattern) (cols pattern))))
 
 (defn part1
   [patterns]
   (apply + (map score patterns)))
 
-(let [input input #_(slurp "13.txt")
+(let [input #_input (slurp "13.txt")
       patterns (s/split input #"\n\n")]
   (println (part1 patterns)))
