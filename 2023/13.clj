f34714b8f02762adfd7b9da8d1aee614aca95097
Paul Madden <paul.madden@noaa.gov>
2023-12-23 05:47:54 +0000

    Cleanup

--- 2023/13.clj
+++ 2023/13.clj
@@ -27,32 +27,23 @@
 (defn fwd
   ([lines] (fwd lines 0))
   ([lines c]
-   #_(print c)
-   #_(pprint lines)
    (if (seq lines)
      (let [n (count lines)]
        (if (= (mod n 2) 0)
-         (do #_(print "a") #_(pprint (subvec lines 0 (/ n 2)))
-             #_(print "b") #_(pprint (reverse (subvec lines (/ n 2))))
-             (if (= (subvec lines 0 (/ n 2)) (reverse (subvec lines (/ n 2))))
-               (+ c (/ n 2))
-               (fwd (vec (drop 2 lines))  (+ c 2)))
-             )
+         (if (= (subvec lines 0 (/ n 2)) (reverse (subvec lines (/ n 2))))
+           (+ c (/ n 2))
+           (fwd (vec (drop 2 lines))  (+ c 2)))
          (fwd (vec (drop 1 lines)) (+ c 1))))
      0)))
 
 (defn rev
   [lines]
-  #_(pprint lines)
   (if (seq lines)
     (let [n (count lines)]
       (if (= (mod n 2) 0)
-        (do #_(print "a") #_(pprint (subvec lines 0 (/ n 2)))
-            #_(print "b") #_(pprint (reverse (subvec lines (/ n 2))))
-            (if (= (subvec lines 0 (/ n 2)) (reverse (subvec lines (/ n 2))))
-              (/ n 2)
-              (rev (vec (drop-last 2 lines))))
-            )
+        (if (= (subvec lines 0 (/ n 2)) (reverse (subvec lines (/ n 2))))
+          (/ n 2)
+          (rev (vec (drop-last 2 lines))))
         (rev (vec (drop-last 1 lines)))))
     0))
 
@@ -61,16 +52,12 @@
 (defn rows
   [pattern]
   (let [lines (s/split pattern #"\n")]
-    #_(println "rows")
-    #_(pprint lines)
     (* 100 (+ (fwd lines)
               (if (= lines (reverse lines)) 0 (rev lines))))))
 
 (defn cols
   [pattern]
   (let [lines (s/split (transpose pattern) #"\n")]
-    #_(println "cols")
-    #_(pprint lines)
     (+ (fwd lines)
        (if (= lines (reverse lines)) 0 (rev lines)))))
 
@@ -78,8 +65,6 @@
   [pattern]
   (let [sr (rows pattern)
         sc (cols pattern)]
-    #_(println "rows" sr "cols" sc)
-    #_(read-line)
     (+ (rows pattern) (cols pattern))))
 
 (defn part1
