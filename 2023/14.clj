14eeca2316fd3c0e2e1af53e6522c3ea2be1c0a0
Paul Madden <maddenp@colorado.edu>
2023-12-27 19:59:42 +0000

    Work on 14.2

--- 2023/14.clj
+++ 2023/14.clj
@@ -18,7 +18,7 @@
        (apply mapv vector)
        (map #(apply str %))))
 
-(defn score
+(defn weigh
   [line]
   (let [n (count line)]
     (loop [i 0 x 0 load 0]
@@ -30,10 +30,34 @@
             (recur j x load)))
         load))))
 
+(defn swap
+  [s a b]
+  (if (= a b)
+    s
+    (let [[a b] (sort [a b])]
+      (str (subs s 0 a) (get s b) (subs s (inc a) b) (get s a) (subs s (inc b))))))
+
+(defn tilt
+  [line]
+  (let [n (count line)]
+    (loop [i 0 x 0 s line]
+      (if (< i n)
+        (let [j (inc i)]
+          (case (get line i)
+            \O (recur j (inc x) (swap s i x))
+            \# (recur j j s)
+            (recur j x s)))
+        s))))
+
 (defn part1
   [input]
   (let [lines (transpose (s/split input #"\n"))]
-    (apply + (map score lines))))
+    (apply + (map weigh lines))))
 
-(let [input #_input (slurp "14.txt")]
-  (println (part1 input)))
+(defn part2
+  [input]
+  (let [lines (s/split input #"\n")]
+    (s/join "\n" (transpose (map tilt (transpose lines))))))
+
+(let [input input #_(slurp "14.txt")]
+  (println #_(part1 input) (part2 input)))
