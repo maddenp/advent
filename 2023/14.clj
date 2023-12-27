1c4214ceb70e36a5f586b8971fbfa54bb2292e0f
Paul Madden <maddenp@colorado.edu>
2023-12-27 02:42:14 +0000

    Solves 14.1

--- 2023/14.clj
+++ 2023/14.clj
@@ -11,9 +11,33 @@
                          "#....###.."
                          "#OO..#...."]))
 
+(defn transpose
+  [lines]
+  (->> lines
+       (map #(seq (char-array %)))
+       (apply mapv vector)
+       (map #(apply str %))))
+
+(defn score
+  [line]
+  (let [n (count line)]
+    (loop [i 0 x 0 load 0]
+      #_(println "@@@" (get line i) "i" i "x" x "load" load)
+      #_(read-line)
+      (if (< i n)
+        (let [c (get line i) j (inc i)]
+          (cond (= c \O)
+                (recur j (inc x) (+ load (- n x)))
+                (= c \#)
+                (recur j j load)
+                :else
+                (recur j x load)))
+        load))))
+
 (defn part1
   [input]
-  input)
+  (let [lines (transpose (s/split input #"\n"))]
+    (apply + (map score lines))))
 
-(let [input input #_(slurp "14.txt")]
+(let [input #_input (slurp "14.txt")]
   (println (part1 input)))
