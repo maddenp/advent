95742098aeb3376fc68551f4d566a5891fe65af7
Paul Madden <maddenp@colorado.edu>
2023-12-29 03:51:03 +0000

    Work on 14.2

--- 2023/14.clj
+++ 2023/14.clj
@@ -28,8 +28,7 @@
 
 (defn part1
   [input]
-  (let [lines (transpose (s/split input #"\n"))]
-    (apply + (map weigh lines))))
+  (apply + (map weigh (transpose (s/split input #"\n")))))
 
 (defn tilt
   [line]
@@ -61,10 +60,32 @@
 
 (def spin-cycle (comp tilt-e tilt-s tilt-w tilt-n))
 
-(defn part2
+#_(defn part2
   [input]
   (let [lines (s/split input #"\n")]
-    (apply + (map weigh (nth (iterate spin-cycle lines) 1000000000)))))
+    (loop [lines lines idx 0 idx2lines {} lines2idx {}]
+      (let [next (spin-cycle lines)]
+        (if-let [idx0 (lines2idx next)]
+          (let [cyclen (- idx idx0)
+                a (- 1000000000 idx0)
+                b (mod a cyclen)]
+            (part1 (s/join "\n" (idx2lines (+ idx0 b)))))
+          (recur next (inc idx) (assoc idx2lines idx next) (assoc lines2idx next idx)))))))
+
+(defn part2
+  [input]
+  (apply + (map weigh (transpose (s/split input #"\n"))))
+  #_(let [lines (s/split input #"\n")]
+    (loop [lines lines idx 0]
+      (let [next (spin-cycle lines)
+            weight (apply + (map weigh (transpose next)))]
+        (println idx weight)
+        (if (= weight 64)
+          nil
+          (recur next (inc idx))))))
+  )
+
+;;; (apply + (map weigh (nth (iterate spin-cycle lines) 1000000000)))
 
 (def input (s/join "\n" ["O....#...."
                          "O.OO#....#"
@@ -77,5 +98,7 @@
                          "#....###.."
                          "#OO..#...."]))
 
+(require '[clojure.pprint :refer [pprint]])
+
 (let [input input #_(slurp "14.txt")]
-  (println #_(part1 input) (part2 input)))
+  (pprint #_(part1 input) (part2 input)))
