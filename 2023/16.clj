e3c06a36fed33c9d1e487828d64f15eb4916fd97
Paul Madden <maddenp@colorado.edu>
2023-12-31 22:57:49 +0000

    Cleanup

--- 2023/16.clj
+++ 2023/16.clj
@@ -3,7 +3,14 @@
 (defn cols [a] (alength (aget a 0)))
 (defn rows [a] (alength a))
 
-(defn enqueue
+(defn show
+  [a]
+  (doseq [row (range (rows a))]
+    (doseq [col (range (cols a))]
+      (print (aget a row col)))
+    (println)))
+
+(defn visit
   [seen q a r c dirs]
   (let [offsets {:n [-1 0] :e [0 +1] :s [+1 0] :w [0 -1]}
         xs (for [d' dirs
@@ -12,27 +19,21 @@
              [r' c' d'])]
     (apply conj (rest q) (remove #(seen %) xs))))
 
-(defn show
-  [a]
-  (doseq [row (range (rows a))]
-    (doseq [col (range (cols a))]
-      (print (aget a row col)))
-    (println)))
-
 (defn energize
   [a]
   (let [energized (to-array-2d (repeat (rows a) (repeat (cols a) \.)))]
     (loop [seen #{} q (list [0 0 :e])]
       (if (seq q)
-        (let [[r c d] (first q) visit (partial enqueue seen q a r c)]
+        (let [[r c d] (first q)]
           (aset energized r c \#)
           (recur (conj seen [r c d])
-                 (visit (case (aget a r c)
+                 (visit seen q a r c
+                        (case (aget a r c)
                           \. [d]
-                          \| (case d :n [:n] :e [:n :s] :s [:s] :w [:n :s])
-                          \- (case d :n [:e :w] :e [:e] :s [:e :w] :w [:w])
-                          \/ (case d :n [:e] :e [:n] :s [:w] :w [:s])
-                          \\ (case d :n [:w] :e [:s] :s [:e] :w [:n])))))
+                          \| (case d :n [:n   ] :e [:n :s] :s [:s   ] :w [:n :s])
+                          \- (case d :n [:e :w] :e [:e   ] :s [:e :w] :w [:w   ])
+                          \/ (case d :n [:e   ] :e [:n   ] :s [:w   ] :w [:s   ])
+                          \\ (case d :n [:w   ] :e [:s   ] :s [:e   ] :w [:n   ])))))
         energized))))
 
 (defn part1
