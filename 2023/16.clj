5ffd5c5b0282eaf81c63436e770202068de8e7f4
Paul Madden <maddenp@colorado.edu>
2023-12-31 22:24:56 +0000

    Work on 16.1

--- 2023/16.clj
+++ 2023/16.clj
@@ -4,15 +4,15 @@
 (defn rows [a] (alength a))
 
 (defn enqueue
-  [q a r c dirs]
+  [seen q a r c dirs]
   (let [offsets {:n [-1 0] :e [0 +1] :s [+1 0] :w [0 -1]}
         xs (for [d' dirs
                  :let [[dr dc] (offsets d') r' (+ r dr) c' (+ c dc)]
                  :when (and (<= 0 r' (dec (rows a))) (<= 0 c' (dec (cols a))))]
              [r' c' d'])]
-    (apply conj (rest q) xs)))
+    (apply conj (rest q) (remove #(seen %) xs))))
 
-(defn show ; TODO REMOVE
+(defn show
   [a]
   (doseq [row (range (rows a))]
     (doseq [col (range (cols a))]
@@ -22,20 +22,20 @@
 (defn part1
   [a]
   (let [energized (to-array-2d (repeat (rows a) (repeat (cols a) \.)))]
-    (loop [q (list [0 0 :e])]
+    (loop [seen #{} q (list [0 0 :e])]
       (if (seq q)
-        (let [[r c d] (first q) visit (partial enqueue q a r c)]
+        (let [[r c d] (first q) visit (partial enqueue seen q a r c)]
           (show a)
           (println "r" r "c" c "d" d "q" q)
           (aset energized r c \#)
           (show energized)
           (read-line)
           (case (aget a r c)
-            \. (recur (visit [d]))
-            \| (recur (visit (case d :n [:n] :e [:n :s] :s [:s] :w [:n :s])))
-            \- (recur (visit (case d :n [:e :w] :e [:e] :s [:e :w] :w [:w])))
-            \/ (recur (visit (case d :n [:e] :e [:n] :s [:w] :w [:s])))
-            \\ (recur (visit (case d :n [:w] :e [:s] :s [:e] :w [:n])))))
+            \. (recur (conj seen [r c d]) (visit [d]))
+            \| (recur (conj seen [r c d]) (visit (case d :n [:n] :e [:n :s] :s [:s] :w [:n :s])))
+            \- (recur (conj seen [r c d]) (visit (case d :n [:e :w] :e [:e] :s [:e :w] :w [:w])))
+            \/ (recur (conj seen [r c d]) (visit (case d :n [:e] :e [:n] :s [:w] :w [:s])))
+            \\ (recur (conj seen [r c d]) (visit (case d :n [:w] :e [:s] :s [:e] :w [:n])))))
         energized))))
 
 (defn part2
