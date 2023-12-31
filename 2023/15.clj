f7ba632741bca17409ac01fc8943879d1177dee0
Paul Madden <maddenp@colorado.edu>
2023-12-31 01:14:52 +0000

    Work on 15.2

--- 2023/15.clj
+++ 2023/15.clj
@@ -10,5 +10,32 @@
   [steps]
   (apply + (map h steps)))
 
-(let [steps (s/split (s/trim-newline #_input (slurp "15.txt")) #",")]
-  (println (part1 steps) #_(part2 steps)))
+(defn insert-lens
+  [boxes label fl]
+  (let [i (h label) lenses (boxes i)]
+    (if (some #(= (first %) label) lenses)
+      (assoc boxes i (for [lens lenses] (if (= (first lens) label) [label fl] lens)))
+      (assoc boxes i (conj lenses [label fl])))))
+
+(defn remove-lens
+  [boxes label]
+  (println "@@@" label)
+  (let [i (h label)]
+    (assoc boxes i (vec (remove #(= (first %) label) (boxes i))))))
+
+(defn part2
+  [steps]
+  (let [boxes (reduce merge (mapv #(hash-map % []) (range 256)))]
+    (loop [steps steps boxes boxes]
+      (if (seq steps)
+        (let [[label fl] (s/split (first steps) #"[-=]")]
+          (do (println "label" label "i" (h label) "fl" fl)
+              (if fl
+                (recur (rest steps) (insert-lens boxes label fl))
+                (recur (rest steps) (remove-lens boxes label)))))
+        (remove #(empty? (last %)) boxes)))))
+
+(require '[clojure.pprint :refer [pprint]])
+
+(let [steps (s/split (s/trim-newline input #_(slurp "15.txt")) #",")]
+  (pprint #_(part1 steps) (part2 steps)))
