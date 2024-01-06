becf0e79ec75264e5043340302625a1f2d5d7030
Paul Madden <paul.madden@noaa.gov>
2024-01-06 21:16:07 +0000

    Use ubergraph for d16

--- src/advent/2023/d16.clj
+++ src/advent/2023/d16.clj
@@ -1,8 +1,9 @@
 (ns advent.2023.d16
-  (:require [advent.common :refer [cols rows]]
+  (:require [advent.common :refer [cols rows show]]
             [clojure.java.io :as io]
             [clojure.string :as s]
-            [ubergraph.core :as uber]))
+            [ubergraph.alg :refer [shortest-path]]
+            [ubergraph.core :refer [digraph]]))
 
 (defn dirs
   [a r c d]
@@ -13,7 +14,7 @@
     \/ (case d :n [:e   ] :e [:n   ] :s [:w   ] :w [:s   ])
     \\ (case d :n [:w   ] :e [:s   ] :s [:e   ] :w [:n   ])))
 
-(defn visit
+(defn neighbors
   [a r c d]
   (let [offsets {:n [-1 0] :e [0 +1] :s [+1 0] :w [0 -1]}]
     (for [d' (dirs a r c d)
@@ -21,44 +22,35 @@
           :when (and (<= 0 r' (dec (rows a))) (<= 0 c' (dec (cols a))))]
       [r' c' d'])))
 
-(defn q'
-  [seen q a r c d]
-  (apply conj (rest q) (remove #(seen %) (visit a r c d))))
-
-(defn energize
-  [a r c d]
-  (loop [q (list [r c d]) energized #{} seen #{}]
-    (if (seq q)
-      (let [[r c d] (first q)]
-        (recur (q' seen q a r c d) (conj energized [r c]) (conj seen [r c d])))
-      (count energized))))
+(defn tally
+  [g node]
+  (count (set (map #(take 2 %) (keys (:depths (shortest-path g {:start-node node})))))))
 
 (defn part1
-  [a]
-  (energize a 0 0 :e))
+  [g]
+  (tally g [0 0 :e]))
 
 (defn part2
-  [a]
+  [g a]
   (let [ra (rows a) ca (cols a) rs (range ra) cs (range ca)]
     (->> (for [[r c d] (concat (map #(vector 0 % :s) cs)
                                (map #(vector % (dec ca) :w) (rest rs))
                                (map #(vector (dec ra) % :n) (rest (reverse cs)))
                                (map #(vector % 0 :e) (rest (reverse (rest rs)))))]
-           (energize a r c d))
+           (tally g [r c d]))
          (apply max))))
 
 (defn go
   [& _]
-  (let [a (to-array-2d (s/split (slurp (io/resource "resources/2023/d16.txt")) #"\n"))]
-    [(part1 a) (part2 a)]))
+  (let [a (to-array-2d (s/split (slurp (io/resource "resources/2023/d16.txt")) #"\n"))
+        g (apply digraph
+                 (for [r (range (rows a))
+                       c (range (cols a))
+                       d [:n :e :s :w]
+                       n (neighbors a r c d)]
+                   [[r c d] n]))]
+    [(part1 g) (part2 g a)]))
 
 (defn -main
   [& _]
   (apply println (go)))
-
-#_(defn show
-    [a]
-    (doseq [row (range (rows a))]
-      (doseq [col (range (cols a))]
-        (print (aget a row col)))
-      (println)))
